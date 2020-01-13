22122e796a7d6297ad3561359965ac404fbcfcff
Jen Hail <jen@mightily.com>
2020-01-13 18:10:25 +0000

    add code in files types: .css,.map,.js,.php,.scss remove code in files types: .css,.map,.js,.php

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("cvbsl")
-console.log("mufag")
+console.log("dvebt")
--- content.php
+++ content.php
@@ -15,5 +15,5 @@
         echo "qcdiv";
         echo "phign";
         echo "locbt";
-        echo "nfsjf";
+        echo "vkkhv";
 ?>
