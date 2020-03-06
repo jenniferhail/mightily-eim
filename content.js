cf6b7e7e4d0d8b9df6a2398b8739c0525b90157e
Jen Hail <jen@mightily.com>
2020-03-06 19:07:16 +0000

    add code in files types: .js,.php remove code in files types: .js,.php

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("cvbsl")
-console.log("hxyam")
+console.log("ihlsf")
--- content.php
+++ content.php
@@ -16,5 +16,5 @@
         echo "hhgve";
         echo "gtoul";
         echo "bcfux";
-        echo "vbupj";
+        echo "ceuln";
 ?>
